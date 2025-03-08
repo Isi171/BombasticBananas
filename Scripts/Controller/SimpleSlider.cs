efd3f73474fc21156f4ff4d135d6ee083277014f
Filippo Agalbato <filippo.aga@gmail.com>
2025-03-08 10:05:43 +0100

    make guardrail slide infinitely

---
+++ Scripts/Controller/SimpleSlider.cs
@@ -0,0 +1,24 @@
+using Godot;
+
+public partial class SimpleSlider : RigidBody2D
+{
+    private Vector2 originalPosition;
+
+    public override void _Ready()
+    {
+        originalPosition = GlobalPosition;
+    }
+
+    public override void _Process(double delta)
+    {
+        if (GlobalPosition.X <= -1920)
+        {
+            GlobalPosition = originalPosition;
+        }
+    }
+
+    public override void _IntegrateForces(PhysicsDirectBodyState2D state)
+    {
+        state.LinearVelocity = new Vector2(-400, 0);
+    }
+}
