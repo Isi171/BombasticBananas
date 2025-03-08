41353b7bb595b194b38a90255cf23340396b3192
Filippo Agalbato <filippo.aga@gmail.com>
2025-03-08 16:10:25 +0100

    reformat file

--- Scripts/Controller/MovementController.cs
+++ Scripts/Controller/MovementController.cs
@@ -2,100 +2,99 @@ using Godot;
 
 namespace BombasticBananas.Scripts.Controller
 {
-	public partial class MovementController : RigidBody2D
-	{
-		private const float SlidingLeftForce = -300f;
-		private const float MaxSlidingLeftVelocity = -400f;
-		private const float MaxSlidingRightVelocity = 300f;
-		private const float JumpForce = -600f;
-		private const float MovementForce = 100f;
+    public partial class MovementController : RigidBody2D
+    {
+        private const float SlidingLeftForce = -300f;
+        private const float MaxSlidingLeftVelocity = -400f;
+        private const float MaxSlidingRightVelocity = 300f;
+        private const float JumpForce = -600f;
+        private const float MovementForce = 100f;
 
-		private const string RightFingerAction = "RightFinger";
-		private const string LeftFingerAction = "LeftFinger";
-		private string lastInput;
+        private const string RightFingerAction = "RightFinger";
+        private const string LeftFingerAction = "LeftFinger";
+        private string lastInput;
 
-		private Area2D groundChecker;
-		private bool isJumping;
-		private bool isDescendingDuringJump;
+        private Area2D groundChecker;
+        private bool isJumping;
+        private bool isDescendingDuringJump;
 
-		private const double DescendColliderEnableTimer = 0.3;
-		private bool isDescendingBetweenLayers;
-		
-		private const float IndexMaxAngle = -20;
-		private const float IndexMinAngle = 55;
-		private const float FuckMaxAngle = -20;
-		private const float FuckMinAngle = 40;
-		private const float FingerSpeed = 10;
-		
-		private Bone2D index;
-		private Bone2D fuck;
+        private const double DescendColliderEnableTimer = 0.3;
+        private bool isDescendingBetweenLayers;
 
-		public override void _Ready()
-		{
-			AddConstantForce(new Vector2(SlidingLeftForce, 0));
-			groundChecker = GetNode<Area2D>("GroundChecker");
-			index = GetNode<Bone2D>("Visuals/Skeleton2D/Hand/Index");
-			fuck = GetNode<Bone2D>("Visuals/Skeleton2D/Hand/Fuck");
-		}
+        private const float IndexMaxAngle = -20;
+        private const float IndexMinAngle = 55;
+        private const float FuckMaxAngle = -20;
+        private const float FuckMinAngle = 40;
 
-		public override void _PhysicsProcess(double delta)
-		{
-			bool isTouchingGround = groundChecker.GetOverlappingBodies().Count > 0;
-			isDescendingDuringJump = isJumping && LinearVelocity.Y < 0;
+        private Bone2D index;
+        private Bone2D fuck;
 
-			if (isDescendingDuringJump && isTouchingGround)
-			{
-				isJumping = false;
-				isDescendingDuringJump = false;
-			}
+        public override void _Ready()
+        {
+            AddConstantForce(new Vector2(SlidingLeftForce, 0));
+            groundChecker = GetNode<Area2D>("GroundChecker");
+            index = GetNode<Bone2D>("Visuals/Skeleton2D/Hand/Index");
+            fuck = GetNode<Bone2D>("Visuals/Skeleton2D/Hand/Fuck");
+        }
 
-			if (Input.IsActionJustPressed("Jump") && isTouchingGround && !isJumping)
-			{
-				isJumping = true;
-				LinearVelocity = new Vector2(LinearVelocity.X, 0);
-				ApplyImpulse(new Vector2(0, JumpForce));
-			}
+        public override void _PhysicsProcess(double delta)
+        {
+            bool isTouchingGround = groundChecker.GetOverlappingBodies().Count > 0;
+            isDescendingDuringJump = isJumping && LinearVelocity.Y < 0;
 
-			if (Input.IsActionJustPressed(RightFingerAction) && lastInput != RightFingerAction)
-			{
-				lastInput = RightFingerAction;
-				ApplyImpulse(new Vector2(MovementForce, 0));
-				
-				index.RotationDegrees = IndexMaxAngle;
-				fuck.RotationDegrees = FuckMinAngle;
-			}
+            if (isDescendingDuringJump && isTouchingGround)
+            {
+                isJumping = false;
+                isDescendingDuringJump = false;
+            }
 
-			if (Input.IsActionJustPressed(LeftFingerAction) && lastInput != LeftFingerAction)
-			{
-				lastInput = LeftFingerAction;
-				ApplyImpulse(new Vector2(MovementForce, 0));
-				
-				index.RotationDegrees =  IndexMinAngle;
-				fuck.RotationDegrees =  FuckMaxAngle;
-			}
-		   
+            if (Input.IsActionJustPressed("Jump") && isTouchingGround && !isJumping)
+            {
+                isJumping = true;
+                LinearVelocity = new Vector2(LinearVelocity.X, 0);
+                ApplyImpulse(new Vector2(0, JumpForce));
+            }
 
-			if (Input.IsActionJustPressed("Descend") && !isDescendingBetweenLayers)
-			{
-				isDescendingBetweenLayers = true;
-				SetCollisionLayerValue(3, false);
-				SetCollisionMaskValue(3, false);
-				GetTree().CreateTimer(DescendColliderEnableTimer).Timeout += ReEnableDescendCollision;
-			}
-		}
+            if (Input.IsActionJustPressed(RightFingerAction) && lastInput != RightFingerAction)
+            {
+                lastInput = RightFingerAction;
+                ApplyImpulse(new Vector2(MovementForce, 0));
 
-		private void ReEnableDescendCollision()
-		{
-			isDescendingBetweenLayers = false;
-			SetCollisionLayerValue(3, true);
-			SetCollisionMaskValue(3, true);
-		}
+                index.RotationDegrees = IndexMaxAngle;
+                fuck.RotationDegrees = FuckMinAngle;
+            }
 
-		public override void _IntegrateForces(PhysicsDirectBodyState2D state)
-		{
-			state.LinearVelocity = new Vector2(
-				Mathf.Clamp(state.LinearVelocity.X, MaxSlidingLeftVelocity, MaxSlidingRightVelocity),
-				state.LinearVelocity.Y);
-		}
-	}
+            if (Input.IsActionJustPressed(LeftFingerAction) && lastInput != LeftFingerAction)
+            {
+                lastInput = LeftFingerAction;
+                ApplyImpulse(new Vector2(MovementForce, 0));
+
+                index.RotationDegrees = IndexMinAngle;
+                fuck.RotationDegrees = FuckMaxAngle;
+            }
+
+
+            if (Input.IsActionJustPressed("Descend") && !isDescendingBetweenLayers)
+            {
+                isDescendingBetweenLayers = true;
+                SetCollisionLayerValue(3, false);
+                SetCollisionMaskValue(3, false);
+                GetTree().CreateTimer(DescendColliderEnableTimer).Timeout += ReEnableDescendCollision;
+            }
+        }
+
+        private void ReEnableDescendCollision()
+        {
+            isDescendingBetweenLayers = false;
+            SetCollisionLayerValue(3, true);
+            SetCollisionMaskValue(3, true);
+        }
+
+        public override void _IntegrateForces(PhysicsDirectBodyState2D state)
+        {
+            state.LinearVelocity = new Vector2(
+                Mathf.Clamp(state.LinearVelocity.X, MaxSlidingLeftVelocity, MaxSlidingRightVelocity),
+                state.LinearVelocity.Y);
+        }
+    }
 }
