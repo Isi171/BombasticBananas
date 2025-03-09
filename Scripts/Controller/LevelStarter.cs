13539e6a9cf797366f760cb77a6e154236c8034d
Filippo Agalbato <filippo.aga@gmail.com>
2025-03-09 14:45:21 +0100

    reformat whitespace

--- Scripts/Controller/LevelStarter.cs
+++ Scripts/Controller/LevelStarter.cs
@@ -2,43 +2,42 @@ using Godot;
 
 namespace BombasticBananas.Scripts.Controller
 {
-	public partial class LevelStarter : Node
-	{
-		private RigidBody2D fingers;
-		private Control mainMenuUi;
-		private Control RunUI;
-		private Control JumpUI;
+    public partial class LevelStarter : Node
+    {
+        private RigidBody2D fingers;
+        private Control mainMenuUi;
+        private Control RunUI;
+        private Control JumpUI;
 
-		private bool isOnMainMenu;
+        private bool isOnMainMenu;
 
-		public override void _Ready()
-		{
-			fingers = GetNode<RigidBody2D>("fingers");
-			mainMenuUi = GetNode<Control>("mainMenuUi");
-			RunUI = GetNode<Control>("Tutorial/Run");
-			JumpUI = GetNode<Control>("Tutorial/Jump");
-			isOnMainMenu = true;
-		}
+        public override void _Ready()
+        {
+            fingers = GetNode<RigidBody2D>("fingers");
+            mainMenuUi = GetNode<Control>("mainMenuUi");
+            RunUI = GetNode<Control>("Tutorial/Run");
+            JumpUI = GetNode<Control>("Tutorial/Jump");
+            isOnMainMenu = true;
+        }
 
-		public override void _Process(double delta)
-		{
-			if (isOnMainMenu && Input.IsAnythingPressed())
-			{
-				isOnMainMenu = false;
-				mainMenuUi.QueueFree();
-				fingers.ProcessMode = ProcessModeEnum.Inherit;
-				fingers.Visible = true;
-				
-				Tween tweenUI = GetTree().CreateTween();
-				tweenUI.TweenProperty(RunUI, "modulate", new Color(1,1,1,1), .5f);
-				tweenUI.TweenInterval(4f);
-				tweenUI.TweenProperty(RunUI, "modulate", new Color(1,1,1,0), 1.5f);
-				tweenUI.TweenInterval(0.5f);
-				tweenUI.TweenProperty(JumpUI, "modulate", new Color(1,1,1,1), .5f);
-				tweenUI.TweenInterval(5f);
-				tweenUI.TweenProperty(JumpUI, "modulate", new Color(1,1,1,0), 1.5f);
-				
-			}
-		}
-	}
+        public override void _Process(double delta)
+        {
+            if (isOnMainMenu && Input.IsAnythingPressed())
+            {
+                isOnMainMenu = false;
+                mainMenuUi.QueueFree();
+                fingers.ProcessMode = ProcessModeEnum.Inherit;
+                fingers.Visible = true;
+
+                Tween tweenUI = GetTree().CreateTween();
+                tweenUI.TweenProperty(RunUI, "modulate", new Color(1, 1, 1, 1), .5f);
+                tweenUI.TweenInterval(4f);
+                tweenUI.TweenProperty(RunUI, "modulate", new Color(1, 1, 1, 0), 1.5f);
+                tweenUI.TweenInterval(0.5f);
+                tweenUI.TweenProperty(JumpUI, "modulate", new Color(1, 1, 1, 1), .5f);
+                tweenUI.TweenInterval(5f);
+                tweenUI.TweenProperty(JumpUI, "modulate", new Color(1, 1, 1, 0), 1.5f);
+            }
+        }
+    }
 }
